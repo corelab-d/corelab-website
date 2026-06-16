b2386cb4017abdda3d953d6b3c829929f72cc748
Omar J Kuri <omarjkuri@MacBook-Air-2.local>
2026-06-16 16:07:47 -0600

    Add LinkedIn Insight Tag to all pages via site.js

--- js/site.js
+++ js/site.js
@@ -78,6 +78,10 @@
     </div>
   </footer>`;
 
+  // LinkedIn Insight Tag
+  (function(l){if(!l){window.lintrk=function(a,b){window.lintrk.q.push([a,b])};window.lintrk.q=[]}var s=document.getElementsByTagName("script")[0];var b=document.createElement("script");b.type="text/javascript";b.async=true;b.src="https://snap.licdn.com/li.lms-analytics/insight.min.js";s.parentNode.insertBefore(b,s);})(window.lintrk);
+  window._linkedin_partner_id="9252194";window._linkedin_data_partner_ids=window._linkedin_data_partner_ids||[];window._linkedin_data_partner_ids.push(window._linkedin_partner_id);
+
   document.addEventListener('DOMContentLoaded', function () {
     // Inject nav before first element
     document.body.insertAdjacentHTML('afterbegin', navHTML);
